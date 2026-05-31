507b5b2853f0ced67191598f3fbffc49ef001a0a
Your Name <you@example.com>
2026-05-31 13:45:45 +0300

    Add authentication pages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from accounts.forms import UserRegisterForm
+
+
+class RegisterView(CreateView):
+    form_class = UserRegisterForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('ticket_list')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+
+        return response
